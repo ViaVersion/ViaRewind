805b4b3736f50da247c1dd5a98bd1ecdd8797f9c
Gerrygames <guenterwalklol@aim.com>
2018-02-08 21:39:02 +0100

    better unused color regex pattern

--- core/src/main/java/de/gerrygames/viarewind/utils/ChatUtil.java
+++ core/src/main/java/de/gerrygames/viarewind/utils/ChatUtil.java
@@ -3,7 +3,10 @@ package de.gerrygames.viarewind.utils;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
 
+import java.util.regex.Pattern;
+
 public class ChatUtil {
+	private static Pattern unusedColorPattern = Pattern.compile("(?>(?>§[0-fk-or])*(§r|\\Z))|(?>(?>§[0-f])*(§[0-f]))");
 
 	public static String jsonToLegacy(String json) {
 		if (json==null || json.equals("null")) return "";
@@ -19,9 +22,6 @@ public class ChatUtil {
 
 	public static String removeUnusedColor(String legacy) {
 		if (legacy==null) return null;
-		legacy = legacy.replaceAll("§[0-f](§[0-f|r])", "$1");
-		legacy = legacy.replaceAll("(§.)?\\1", "$1");
-		legacy = legacy.replaceAll("(§.)*\\Z", "");
-		return legacy;
+		return unusedColorPattern.matcher(legacy).replaceAll("$1$2");
 	}
 }
