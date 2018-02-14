cf1270d4e58c19e170bb9ce78272f14308b9fca0
Gerrygames <guenterwalklol@aim.com>
2018-02-14 13:31:48 +0100

    rewrite chunk transformation

--- core/src/main/java/de/gerrygames/viarewind/protocol/protocol1_7_6_10to1_8/chunks/ChunkPacketTransformer.java
+++ core/src/main/java/de/gerrygames/viarewind/protocol/protocol1_7_6_10to1_8/chunks/ChunkPacketTransformer.java
@@ -2,6 +2,8 @@ package de.gerrygames.viarewind.protocol.protocol1_7_6_10to1_8.chunks;
 
 import de.gerrygames.viarewind.protocol.protocol1_7_6_10to1_8.items.ReplacementRegistry1_7_6_10to1_8;
 import de.gerrygames.viarewind.storage.BlockStorage;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import us.myles.ViaVersion.api.PacketWrapper;
 import us.myles.ViaVersion.api.minecraft.Environment;
 import us.myles.ViaVersion.api.type.Type;
@@ -22,15 +24,13 @@ public class ChunkPacketTransformer {
 		CustomByteType customByteType = new CustomByteType(size);
 		byte[] data = packetWrapper.read(customByteType);
 
-		Chunk1_7_6_10to1_8 chunk = new Chunk1_7_6_10to1_8(data, primaryBitMask, world==null || world.getEnvironment()==Environment.NORMAL, groundUp);
+		data = transformChunkData(data, primaryBitMask, world==null || world.getEnvironment()==Environment.NORMAL, groundUp);
 
 		packetWrapper.write(Type.INT, chunkX);
 		packetWrapper.write(Type.INT, chunkZ);
-		packetWrapper.write(Type.BOOLEAN, chunk.groundUp);
+		packetWrapper.write(Type.BOOLEAN, groundUp);
 		packetWrapper.write(Type.SHORT, (short) primaryBitMask);
 		packetWrapper.write(Type.SHORT, (short) 0);
-
-		data = chunk.get1_7Data();
 
 		packetWrapper.write(Type.INT, data.length);
 
@@ -51,6 +51,59 @@ public class ChunkPacketTransformer {
 		packetWrapper.write(customByteType, compressedData);
 	}
 
+	private static byte[] transformChunkData(byte[] data, int primaryBitMask, boolean skyLight, boolean groundUp) {
+		int dataSize = 0;
+
+		ByteBuf buf = Unpooled.buffer();
+		ByteBuf blockDataBuf = Unpooled.buffer();
+
+		for (int i = 0; i < 16; i++) {
+			if ((primaryBitMask & 1 << i) == 0) continue;
+			byte tmp = 0;
+			for (int j = 0; j < 4096; j++) {
+				short blockData = (short) ((data[(dataSize + 1)] & 0xFF) << 8 | data[dataSize] & 0xFF);
+				dataSize += 2;
+
+				BlockStorage.BlockState state = BlockStorage.rawToState(blockData);
+				state = ReplacementRegistry1_7_6_10to1_8.replace(state);
+
+				buf.writeByte(state.getId());
+
+				if (j % 2 ==0) {
+					tmp = (byte) (state.getData() & 0xF);
+				} else {
+					blockDataBuf.writeByte(tmp | (state.getData() & 15) << 4);
+				}
+			}
+		}
+
+		buf.writeBytes(blockDataBuf);
+		blockDataBuf.release();
+
+		int columnCount = Integer.bitCount(primaryBitMask);
+
+		//Block light
+		buf.writeBytes(data, dataSize, 2048 * columnCount);
+		dataSize += 2048 * columnCount;
+
+		//Sky light
+		if (skyLight) {
+			buf.writeBytes(data, dataSize, 2048 * columnCount);
+			dataSize += 2048 * columnCount;
+		}
+
+		if (groundUp && dataSize+256<=data.length) {
+			buf.writeBytes(data, dataSize, 256);
+			dataSize += 256;
+		}
+
+		data = new byte[buf.readableBytes()];
+		buf.readBytes(data);
+		buf.release();
+
+		return data;
+	}
+
 	private static int calcSize(int i, boolean flag, boolean flag1) {
 		int j = i * 2 * 16 * 16 * 16;
 		int k = i * 16 * 16 * 16 / 2;
@@ -66,20 +119,19 @@ public class ChunkPacketTransformer {
 		int[] chunkX = new int[columnCount];
 		int[] chunkZ = new int[columnCount];
 		int[] primaryBitMask = new int[columnCount];
-		int[] size = new int[columnCount];
 		byte[][] data = new byte[columnCount][];
 
 		for (int i = 0; i < columnCount; i++) {
 			chunkX[i] = packetWrapper.read(Type.INT);
 			chunkZ[i] = packetWrapper.read(Type.INT);
 			primaryBitMask[i] = packetWrapper.read(Type.UNSIGNED_SHORT);
-			size[i] = calcSize(Integer.bitCount(primaryBitMask[i]), skyLightSent, true);
 		}
 
 		int totalSize = 0;
 		for (int i = 0; i < columnCount; i++) {
-			CustomByteType customByteType = new CustomByteType(size[i]);
-			data[i] = new Chunk1_7_6_10to1_8(packetWrapper.read(customByteType), primaryBitMask[i], skyLightSent, true).get1_7Data();
+			int size = calcSize(Integer.bitCount(primaryBitMask[i]), skyLightSent, true);
+			CustomByteType customByteType = new CustomByteType(size);
+			data[i] = transformChunkData(packetWrapper.read(customByteType), primaryBitMask[i], skyLightSent, true);
 			totalSize += data[i].length;
 		}
 
--- core/src/main/java/de/gerrygames/viarewind/protocol/protocol1_8to1_9/chunks/ChunkPacketTransformer.java
+++ core/src/main/java/de/gerrygames/viarewind/protocol/protocol1_8to1_9/chunks/ChunkPacketTransformer.java
@@ -1,6 +1,8 @@
 package de.gerrygames.viarewind.protocol.protocol1_8to1_9.chunks;
 
 import de.gerrygames.viarewind.protocol.protocol1_8to1_9.Protocol1_8TO1_9;
+import de.gerrygames.viarewind.protocol.protocol1_8to1_9.items.ReplacementRegistry1_8to1_9;
+import de.gerrygames.viarewind.storage.BlockStorage;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import us.myles.ViaVersion.api.PacketWrapper;
@@ -12,6 +14,7 @@ import us.myles.ViaVersion.api.minecraft.chunks.ChunkSection;
 import us.myles.ViaVersion.api.type.Type;
 import us.myles.ViaVersion.api.type.types.CustomByteType;
 import us.myles.ViaVersion.exception.CancelException;
+import us.myles.ViaVersion.protocols.protocol1_9_3to1_9_1_2.chunks.ChunkSection1_9_1_2;
 import us.myles.ViaVersion.protocols.protocol1_9_3to1_9_1_2.storage.ClientWorld;
 import us.myles.ViaVersion.protocols.protocol1_9_3to1_9_1_2.types.Chunk1_9_1_2Type;
 
@@ -20,39 +23,84 @@ public class ChunkPacketTransformer {
 		ClientWorld world = packetWrapper.user().get(ClientWorld.class);
 
 		Chunk chunk1_9 = packetWrapper.read(new Chunk1_9_1_2Type(world));
-		boolean skyLight = world.getEnvironment()==Environment.NORMAL;
+		boolean skyLight = world.getEnvironment() == Environment.NORMAL;
 		int primaryBitMask = chunk1_9.getBitmask();
 
-		ByteBuf data = Unpooled.buffer();
-		for (int i = 0; i < chunk1_9.getSections().length; i++) {
-			if ((primaryBitMask & 1 << i) != 0) {
-				ChunkSection section = chunk1_9.getSections()[i];
-				section.writeBlocks(data);
-				section.writeBlockLight(data);
-				if (skyLight) section.writeSkyLight(data);
+		ByteBuf buf = Unpooled.buffer();
+
+		if (chunk1_9.isGroundUp() && primaryBitMask == 0) {
+			primaryBitMask = 65535;
+			for (int i = 0; i<chunk1_9.getSections().length; i++) {
+				ChunkSection1_9_1_2 chunkSection = new ChunkSection1_9_1_2();
+				chunk1_9.getSections()[i] = chunkSection;
+				if (skyLight) chunkSection.setSkyLight(new byte[ChunkSection1_9_1_2.LIGHT_LENGTH]);
 			}
 		}
-		byte[] rawdata = new byte[data.readableBytes()];
-		data.readBytes(rawdata);
-		data.release();
 
-		Chunk1_8to1_9 chunk = new Chunk1_8to1_9(rawdata, primaryBitMask, skyLight, chunk1_9.isGroundUp(), chunk1_9.getBiomeData());
+		for (int i = 0; i < chunk1_9.getSections().length; i++) {
+			if ((primaryBitMask & 1 << i) == 0) continue;
+			ChunkSection section = chunk1_9.getSections()[i];
+			for (int y = 0; y < 16; y++) {
+				for (int z = 0; z < 16; z++) {
+					for (int x = 0; x < 16; x++) {
+						int raw = section.getBlock(x, y, z);
+						BlockStorage.BlockState state = BlockStorage.rawToState(raw);
+						state = ReplacementRegistry1_8to1_9.replace(state);
+
+						char val = (char) (state.getId() << 4 | state.getData());
+						buf.writeByte(val);
+						buf.writeByte(val >> 8);
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < chunk1_9.getSections().length; i++) {
+			if ((primaryBitMask & 1 << i) == 0) continue;
+			ChunkSection section = chunk1_9.getSections()[i];
+			section.writeBlockLight(buf);
+		}
+
+		if (skyLight) {
+			for (int i = 0; i < chunk1_9.getSections().length; i++) {
+				if ((primaryBitMask & 1 << i) == 0) continue;
+				ChunkSection section = chunk1_9.getSections()[i];
+				section.writeSkyLight(buf);
+			}
+		}
+
+		if (chunk1_9.isGroundUp()) {
+			buf.writeBytes(chunk1_9.getBiomeData());
+		}
 
 		final UserConnection user = packetWrapper.user();
-		chunk1_9.getBlockEntities().forEach(nbt ->  {
+		chunk1_9.getBlockEntities().forEach(nbt -> {
 			if (!nbt.contains("x") || !nbt.contains("y") || !nbt.contains("z") || !nbt.contains("id")) return;
-			Position position = new Position((long)(int)nbt.get("x").getValue(), (long)(int)nbt.get("y").getValue(), (long)(int)nbt.get("z").getValue());
-			String id = (String)nbt.get("id").getValue();
+			Position position = new Position((long) (int) nbt.get("x").getValue(), (long) (int) nbt.get("y").getValue(), (long) (int) nbt.get("z").getValue());
+			String id = (String) nbt.get("id").getValue();
 
 			short action;
 			switch (id) {
-				case "minecraft:mob_spawner": action = 1; break;
-				case "minecraft:command_block": action = 2; break;
-				case "minecraft:beacon": action = 3; break;
-				case "minecraft:skull": action = 4; break;
-				case "minecraft:flower_pot": action = 5; break;
-				case "minecraft:banner": action = 6; break;
-				default: return;
+				case "minecraft:mob_spawner":
+					action = 1;
+					break;
+				case "minecraft:command_block":
+					action = 2;
+					break;
+				case "minecraft:beacon":
+					action = 3;
+					break;
+				case "minecraft:skull":
+					action = 4;
+					break;
+				case "minecraft:flower_pot":
+					action = 5;
+					break;
+				case "minecraft:banner":
+					action = 6;
+					break;
+				default:
+					return;
 			}
 
 			PacketWrapper updateTileEntity = new PacketWrapper(0x09, null, user);
@@ -69,16 +117,14 @@ public class ChunkPacketTransformer {
 			}
 		});
 
-		if (chunk1_9.isGroundUp() && primaryBitMask==0) {
-			chunk.primaryBitMask = primaryBitMask = 65535;
-			chunk.fillAir();
-		}
+		byte[] finaldata = new byte[buf.readableBytes()];
+		buf.readBytes(finaldata);
+		buf.release();
 
 		packetWrapper.write(Type.INT, chunk1_9.getX());
 		packetWrapper.write(Type.INT, chunk1_9.getZ());
 		packetWrapper.write(Type.BOOLEAN, chunk1_9.isGroundUp());
 		packetWrapper.write(Type.UNSIGNED_SHORT, primaryBitMask);
-		byte[] finaldata = chunk.get1_8Data();
 		packetWrapper.write(Type.VAR_INT, finaldata.length);
 		packetWrapper.write(new CustomByteType(finaldata.length), finaldata);
 	}
