61899d99fbadcec07e54a2b12f3da367c7be1b57
creeper123123321 <creeper123123321@gmail.com>
2019-03-01 16:59:05 -0300

    add reloadConfig() to ViaRewindConfigImpl

--- core/src/main/java/de/gerrygames/viarewind/api/ViaRewindConfigImpl.java
+++ core/src/main/java/de/gerrygames/viarewind/api/ViaRewindConfigImpl.java
@@ -11,6 +11,7 @@ import java.util.Map;
 public class ViaRewindConfigImpl extends Config implements ViaRewindConfig {
     public ViaRewindConfigImpl(File configFile) {
         super(configFile);
+        reloadConfig();
     }
 
     @Override
