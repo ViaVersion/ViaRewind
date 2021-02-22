261d2b47e81a1e798004e1a35dcd44ea12945fbb
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-02-22 11:14:50 -0300

    fix #269: handle client-side compression, code cleanup

--- core/src/main/java/de/gerrygames/viarewind/protocol/protocol1_7_6_10to1_8/storage/CompressionSendStorage.java
+++ core/src/main/java/de/gerrygames/viarewind/protocol/protocol1_7_6_10to1_8/storage/CompressionSendStorage.java
@@ -1,15 +1,20 @@
 package de.gerrygames.viarewind.protocol.protocol1_7_6_10to1_8.storage;
 
-import lombok.Data;
 import us.myles.ViaVersion.api.data.StoredObject;
 import us.myles.ViaVersion.api.data.UserConnection;
 
-@Data
 public class CompressionSendStorage extends StoredObject {
-
-	private boolean compressionSend = false;
+	private boolean removeCompression = false;
 
 	public CompressionSendStorage(UserConnection user) {
 		super(user);
+	}
+
+	public boolean isRemoveCompression() {
+		return removeCompression;
+	}
+
+	public void setRemoveCompression(boolean removeCompression) {
+		this.removeCompression = removeCompression;
 	}
 }
