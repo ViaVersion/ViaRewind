b0ea49d149c5ec297a01c225ac470747666af392
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-03-26 19:39:26 -0300

    cleanup CompressedNBTType

--- core/src/main/java/de/gerrygames/viarewind/protocol/protocol1_7_6_10to1_8/types/CompressedNBTType.java
+++ core/src/main/java/de/gerrygames/viarewind/protocol/protocol1_7_6_10to1_8/types/CompressedNBTType.java
@@ -1,17 +1,12 @@
 package de.gerrygames.viarewind.protocol.protocol1_7_6_10to1_8.types;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
 import io.netty.buffer.ByteBufOutputStream;
 import us.myles.ViaVersion.api.type.Type;
 import us.myles.viaversion.libs.opennbt.NBTIO;
 import us.myles.viaversion.libs.opennbt.tag.builtin.CompoundTag;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
@@ -22,68 +17,36 @@ public class CompressedNBTType extends Type<CompoundTag> {
 	}
 
 	@Override
-	public CompoundTag read(ByteBuf buffer) {
+	public CompoundTag read(ByteBuf buffer) throws IOException {
 		short length = buffer.readShort();
-		if (length <= 0) {return null;}
-
-		byte[] compressed = new byte[length];
-		buffer.readBytes(compressed);
-		byte[] uncompressed = decompress(compressed);
-
-		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(uncompressed);
-		DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
-		try {
-			return (CompoundTag) NBTIO.readTag((DataInput) dataInputStream);
-		} catch (Throwable throwable) {throwable.printStackTrace();}
-		finally {
-			try {
-				dataInputStream.close();
-			} catch (IOException e) {e.printStackTrace();}
+		if (length <= 0) {
+			return null;
 		}
-		return null;
+
+		ByteBuf compressed = buffer.readSlice(length);
+
+		try (GZIPInputStream gzipStream = new GZIPInputStream(new ByteBufInputStream(compressed))) {
+			return NBTIO.readTag(gzipStream);
+		}
 	}
 
 	@Override
 	public void write(ByteBuf buffer, CompoundTag nbt) throws Exception {
 		if (nbt == null) {
 			buffer.writeShort(-1);
-		} else {
-			ByteBuf buf = buffer.alloc().buffer();
-			ByteBufOutputStream bytebufStream = new ByteBufOutputStream(buf);
-			DataOutputStream dataOutputStream = new DataOutputStream(bytebufStream);
-			NBTIO.writeTag((DataOutput) dataOutputStream, nbt);
-			dataOutputStream.close();
-			byte[] uncompressed = new byte[buf.readableBytes()];
-			buf.readBytes(uncompressed);
-			buf.release();
-			byte[] compressed = compress(uncompressed);
-			buffer.writeShort(compressed.length);
-			buffer.writeBytes(compressed);
+			return;
 		}
-	}
 
-	public static byte[] compress(byte[] content){
-		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-		try{
-			GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream);
-			gzipOutputStream.write(content);
-			gzipOutputStream.close();
-		} catch(IOException e){
-			throw new RuntimeException(e);
-		}
-		return byteArrayOutputStream.toByteArray();
-	}
-
-	public static byte[] decompress(byte[] contentBytes){
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		try{
-			GZIPInputStream stream = new GZIPInputStream(new ByteArrayInputStream(contentBytes));
-			while (stream.available()>0) {
-				out.write(stream.read());
+		ByteBuf compressedBuf = buffer.alloc().buffer();
+		try {
+			try (GZIPOutputStream gzipStream = new GZIPOutputStream(new ByteBufOutputStream(compressedBuf))) {
+				NBTIO.writeTag(gzipStream, nbt);
 			}
-		} catch(IOException e){
-			throw new RuntimeException(e);
+
+			buffer.writeShort(compressedBuf.readableBytes());
+			buffer.writeBytes(compressedBuf);
+		} finally {
+			compressedBuf.release();
 		}
-		return out.toByteArray();
 	}
 }
