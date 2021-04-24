0005eadebc3b16ee435830f58bc05756a260a8b8
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-04-24 11:46:30 -0300

    Fix registering protocols

--- core/src/main/java/de/gerrygames/viarewind/api/ViaRewindPlatform.java
+++ core/src/main/java/de/gerrygames/viarewind/api/ViaRewindPlatform.java
@@ -5,7 +5,6 @@ import de.gerrygames.viarewind.protocol.protocol1_7_0_5to1_7_6_10.Protocol1_7_0_
 import de.gerrygames.viarewind.protocol.protocol1_7_6_10to1_8.Protocol1_7_6_10TO1_8;
 import de.gerrygames.viarewind.protocol.protocol1_8to1_9.Protocol1_8TO1_9;
 import us.myles.ViaVersion.api.Via;
-import us.myles.ViaVersion.api.protocol.ProtocolRegistry;
 import us.myles.ViaVersion.api.protocol.ProtocolVersion;
 
 import java.util.Collections;
@@ -18,9 +17,9 @@ public interface ViaRewindPlatform {
 		String version = ViaRewind.class.getPackage().getImplementationVersion();
 		Via.getManager().getSubPlatforms().add(version != null ? version : "UNKNOWN");
 
-		ProtocolRegistry.registerProtocol(new Protocol1_8TO1_9(), Collections.singletonList(ProtocolVersion.v1_8.getId()), ProtocolVersion.v1_9.getId());
-		ProtocolRegistry.registerProtocol(new Protocol1_7_6_10TO1_8(), Collections.singletonList(ProtocolVersion.v1_7_6.getId()), ProtocolVersion.v1_8.getId());
-		ProtocolRegistry.registerProtocol(new Protocol1_7_0_5to1_7_6_10(), Collections.singletonList(ProtocolVersion.v1_7_1.getId()), ProtocolVersion.v1_7_6.getId());
+		Via.getManager().getProtocolManager().registerProtocol(new Protocol1_8TO1_9(), ProtocolVersion.v1_8, ProtocolVersion.v1_9);
+		Via.getManager().getProtocolManager().registerProtocol(new Protocol1_7_6_10TO1_8(), ProtocolVersion.v1_7_6, ProtocolVersion.v1_8);
+		Via.getManager().getProtocolManager().registerProtocol(new Protocol1_7_0_5to1_7_6_10(), ProtocolVersion.v1_7_1, ProtocolVersion.v1_7_6);
 	}
 
 	Logger getLogger();
