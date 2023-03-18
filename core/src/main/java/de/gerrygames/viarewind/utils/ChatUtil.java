d2fdbba8ddbd38548da904d52665558df23d5fa2
_tomcraft <936063+tomcraft@users.noreply.github.com>
2023-03-18 18:34:59 +0100

    Replace translation keys to raw value in jsonToLegacy (#434)

--- core/src/main/java/de/gerrygames/viarewind/utils/ChatUtil.java
+++ core/src/main/java/de/gerrygames/viarewind/utils/ChatUtil.java
@@ -1,9 +1,15 @@
 package de.gerrygames.viarewind.utils;
 
+import com.viaversion.viaversion.api.protocol.packet.ClientboundPacketType;
 import com.viaversion.viaversion.libs.gson.JsonElement;
+import com.viaversion.viaversion.libs.gson.JsonObject;
+import com.viaversion.viaversion.libs.gson.JsonParser;
 import com.viaversion.viaversion.libs.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import com.viaversion.viaversion.libs.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import com.viaversion.viaversion.protocols.protocol1_12to1_11_1.data.AchievementTranslationMapping;
 import com.viaversion.viaversion.protocols.protocol1_13to1_12_2.ChatRewriter;
+import com.viaversion.viaversion.protocols.protocol1_13to1_12_2.Protocol1_13To1_12_2;
+import com.viaversion.viaversion.rewriter.ComponentRewriter;
 import de.gerrygames.viarewind.ViaRewind;
 
 import java.util.logging.Level;
@@ -11,15 +17,22 @@ import java.util.regex.Pattern;
 
 public class ChatUtil {
 	private static final Pattern UNUSED_COLOR_PATTERN = Pattern.compile("(?>(?>§[0-fk-or])*(§r|\\Z))|(?>(?>§[0-f])*(§[0-f]))");
+	private static final ComponentRewriter<ClientboundPacketType> LEGACY_REWRITER = new ComponentRewriter<ClientboundPacketType>() {
+		@Override
+		protected void handleTranslate(JsonObject object, String translate) {
+			String text = Protocol1_13To1_12_2.MAPPINGS.getMojangTranslation().get(translate);
+			if (text != null) {
+				object.addProperty("translate", text);
+			}
+		}
+	};
 
 	public static String jsonToLegacy(String json) {
 		if (json == null || json.equals("null") || json.isEmpty()) return "";
 		try {
-			String legacy = LegacyComponentSerializer.legacySection().serialize(ChatRewriter.HOVER_GSON_SERIALIZER.deserialize(json));
-			while (legacy.startsWith("§f")) legacy = legacy.substring(2);
-			return legacy;
-		} catch (Exception ex) {
-			ViaRewind.getPlatform().getLogger().log(Level.WARNING, "Could not convert component to legacy text: " + json, ex);
+			return jsonToLegacy(JsonParser.parseString(json));
+		} catch (Exception e) {
+			ViaRewind.getPlatform().getLogger().log(Level.WARNING, "Could not convert component to legacy text: " + json, e);
 		}
 		return "";
 	}
@@ -30,7 +43,15 @@ public class ChatUtil {
 		} else if (component.isJsonPrimitive()) {
 			return component.getAsString();
 		} else {
-			return jsonToLegacy(component.toString());
+			try {
+				LEGACY_REWRITER.processText(component);
+				String legacy = LegacyComponentSerializer.legacySection().serialize(ChatRewriter.HOVER_GSON_SERIALIZER.deserializeFromTree(component));
+				while (legacy.startsWith("§f")) legacy = legacy.substring(2);
+				return legacy;
+			} catch (Exception ex) {
+				ViaRewind.getPlatform().getLogger().log(Level.WARNING, "Could not convert component to legacy text: " + component, ex);
+			}
+			return "";
 		}
 	}
 
