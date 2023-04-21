5f7fdc5b4ee6b9ab5a7a7cb26e4c6871b338851d
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2023-04-21 09:45:52 -0300

    remove 1.7.6 to 1.7.2 vialegacy contains it

--- core/src/main/java/de/gerrygames/viarewind/api/ViaRewindPlatform.java
+++ core/src/main/java/de/gerrygames/viarewind/api/ViaRewindPlatform.java
@@ -5,7 +5,6 @@ import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import de.gerrygames.viarewind.ViaRewind;
 import de.gerrygames.viarewind.protocol.protocol1_7_0_5to1_7_6_10.Protocol1_7_0_5to1_7_6_10;
 import de.gerrygames.viarewind.protocol.protocol1_7_6_10to1_8.Protocol1_7_6_10TO1_8;
-import de.gerrygames.viarewind.protocol.protocol1_7_6to1_7_2.Protocol1_7_6to1_7_2;
 import de.gerrygames.viarewind.protocol.protocol1_8to1_9.Protocol1_8TO1_9;
 
 import java.util.logging.Logger;
@@ -20,8 +19,6 @@ public interface ViaRewindPlatform {
 		Via.getManager().getProtocolManager().registerProtocol(new Protocol1_8TO1_9(), ProtocolVersion.v1_8, ProtocolVersion.v1_9);
 		Via.getManager().getProtocolManager().registerProtocol(new Protocol1_7_6_10TO1_8(), ProtocolVersion.v1_7_6, ProtocolVersion.v1_8);
 		Via.getManager().getProtocolManager().registerProtocol(new Protocol1_7_0_5to1_7_6_10(), ProtocolVersion.v1_7_1, ProtocolVersion.v1_7_6);
-
-		Via.getManager().getProtocolManager().registerProtocol(new Protocol1_7_6to1_7_2(), ProtocolVersion.v1_7_6, ProtocolVersion.v1_7_1);
 	}
 
 	Logger getLogger();
