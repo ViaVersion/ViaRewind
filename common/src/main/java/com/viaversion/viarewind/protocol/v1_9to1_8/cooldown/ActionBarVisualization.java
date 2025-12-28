d50b6a2f89fe9f9f426e6f26e418128f5ab77351
FlorianMichael <git@florianmichael.de>
2025-12-29 00:12:28 +0100

    Prepare for next year (2026)

--- common/src/main/java/com/viaversion/viarewind/protocol/v1_9to1_8/cooldown/ActionBarVisualization.java
+++ common/src/main/java/com/viaversion/viarewind/protocol/v1_9to1_8/cooldown/ActionBarVisualization.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of ViaRewind - https://github.com/ViaVersion/ViaRewind
- * Copyright (C) 2018-2025 ViaVersion and contributors
+ * Copyright (C) 2018-2026 ViaVersion and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
